ac22ae76c5f1b04a4ec646fbe960c17a8eb73675
Alvin Galit <aroblesgalit@gmail.com>
2019-01-23 21:08:50 -0800

    added incrementBy and decrementBy

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,14 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }; 
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
@@ -19,20 +21,19 @@ const store = createStore((state = { count: 0 }, action) => {
     }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
-// Actions - than an object that gets sent to the store
-
-// I'd like to increment the count
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
 
 store.dispatch({
     type: 'INCREMENT'
 });
 
-// RESET - set the count equal to zero
 store.dispatch({
     type: 'RESET'
 });
@@ -41,7 +42,7 @@ store.dispatch({
     type: 'DECREMENT'
 });
 
-
-// I'd like to reset the ocunt to zero
-
-console.log(store.getState());
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
