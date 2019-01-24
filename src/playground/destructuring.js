c5de8094facf8fb66a12c636263012dacedd5c8d
Alvin Galit <aroblesgalit@gmail.com>
2019-01-23 21:45:26 -0800

    practiced more array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -35,8 +35,11 @@
 // Array destructuring
 //
 
-const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city}, ${state}.`);
 
-const [street, city, state, zip] = address;
-
-console.log(`You are in ${city}, ${state}.`);
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+//grab first and third items using array destructuring
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
