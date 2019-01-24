a4b0fa8764d9842e03abe93f2ecca97c95255f39
Alvin Galit <aroblesgalit@gmail.com>
2019-01-23 21:28:59 -0800

    learned renaming property and giving default value

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,11 +1,28 @@
-const person = {
-    name: 'Alvin',
-    age: 33,
-    location: {
-        city: 'Tustin',
-        temp: 68
+// const person = {
+//     name: 'Alvin',
+//     age: 32,
+//     location: {
+//         city: 'Tustin',
+//         temp: 68
+//     }
+// };
+
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
+
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} is ${city}.`);
+// }
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin',
     }
 };
 
-const { name, age } = person;
-console.log(`${name} is ${age}.`);
+const { name: publisherName = 'Self-Published' } = book.publisher;
+
+console.log(publisherName); // Penguin, Self-Published (default)
