6deb92c0ca46d20e8f3684af29cb6885dc89ceb2
Alvin Galit <aroblesgalit@gmail.com>
2019-01-27 12:45:23 -0800

    destructured increment type by defining const incrementCount

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,12 +2,16 @@ import { createStore } from 'redux';
 
 // Action generators - functions that return action objects
 
+const incrementCount = (payload = {}) => ({
+    type: 'INCREMENT',
+    incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+});
+
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
-        case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+        case 'INCREMENT':   
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             }; 
         case 'DECREMENT':
             const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
@@ -31,14 +35,14 @@ const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// });
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
+
+store.dispatch(incrementCount());
 
 store.dispatch({
     type: 'RESET'
