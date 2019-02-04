9b3c2e1f8de28f102b267027a4e01be86ae32b4c
Alvin Galit <aroblesgalit@gmail.com>
2019-02-03 22:48:00 -0800

    imported addExpense and setTextFilter functions into app js file and used store.dispatch to add a couple of expenses for water and gas bills

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
 
 console.log(store.getState());
 
