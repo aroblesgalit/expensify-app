e4aeca80c5c4f6648252b08acc67ce00f19d171c
Alvin Galit <aroblesgalit@gmail.com>
2019-02-03 22:55:35 -0800

    used setTextFilter to just show water bill and used getVisibleExpenses to print visibles ones to screen

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
 
-console.log(store.getState());
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
