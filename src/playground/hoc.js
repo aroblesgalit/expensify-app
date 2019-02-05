f9cc485a3d11f22839d5fabf3ed539d8486147a2
Alvin Galit <aroblesgalit@gmail.com>
2019-02-04 23:43:06 -0800

    defined const requireAuthentication

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -17,12 +17,27 @@ const Info = (props) => (
 const withAdminWarning = (WrappedComponent) => {
     return (props) => (
         <div>
-            <p>This is private info. Please don't share!</p>
-            <WrappedComponent />
+            { props.isAdmin && <p>This is private info. Please don't share!</p>}
+            <WrappedComponent {...props} />
         </div>
     );
 };
 
-const AdminInfo = withAdminWarning(Info);
+// requireAuthentication
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? (
+                <WrappedComponent {...props} /> 
+            ) : (
+                <p>Please log in to view the info</p>
+            )}     
+        </div>
+    );
+}; 
 
-ReactDOM.render(<AdminInfo info="These are the details" />, document.getElementById('app'));
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="These are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the details" />, document.getElementById('app'));
