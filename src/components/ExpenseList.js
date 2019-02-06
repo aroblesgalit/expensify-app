529b0574cb6d0a2c6f4a12ac206f437df40cda28
Alvin Galit <aroblesgalit@gmail.com>
2019-02-05 21:04:47 -0800

    added ExpenseListItem component into the const ExpenseList

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,8 +5,9 @@ import ExpenseListItem from './ExpenseListItem';
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-        {props.expenses.length}
-        <ExpenseListItem />
+        {props.expenses.map((expense) => {
+            return <ExpenseListItem key={expense.id} {...expense} />
+        })}
     </div>
 );
 
