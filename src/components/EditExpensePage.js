d561e423e1e6b8e06611161aaaccb1faf4aaedf3
Alvin Galit <aroblesgalit@gmail.com>
2019-02-10 12:00:53 -0800

    dispatched the action to edit the expense and redirected to the dashboard

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
     return (
         <div>
             <ExpenseForm 
                 expense={props.expense}
-                onSubmit={() => {
+                onSubmit={(expense) => {
+                    props.dispatch(editExpense(props.expense.id, expense));
+                    props.history.push('/');
                     console.log('updated', expense);
                 }}
             />
