b2e0fb2cf88912064c8b44ac5b166248dd13c51c
Alvin Galit <aroblesgalit@gmail.com>
2019-02-13 21:26:53 -0800

    created a test js file for filters from the actions folder, then created a test case for set start date action object

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,14 @@
+import moment from 'moment';
+import { setStartDate, setEndDate } from '../../actions/filters';
+
+test('should generate set start date action object', () => {
+    const action = setStartDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    });
+});
+
+test('should generate set end date action object', () => {
+
+});
