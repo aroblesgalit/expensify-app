94fb2a1c99f67580545990c2d68a184b99438fc6
Alvin Galit <aroblesgalit@gmail.com>
2019-02-23 23:22:51 -0800

    created test case for rendering ExpenseForm correctly with expense data

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -8,3 +8,7 @@ test('should render ExpenseForm correctly', () => {
     expect(wrapper).toMatchSnapshot();
 });
 
+test('should render ExpenseForm correctly with expense data', () => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
+    expect(wrapper).toMatchSnapshot();
+});
