6676ece8375312955357ad9edae7bc6867779dd9
Alvin Galit <aroblesgalit@gmail.com>
2019-02-28 21:44:27 -0800

    refractored EditExpensePage to be a class based component

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,24 +3,46 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-    return (
-        <div>
-            <ExpenseForm 
-                expense={props.expense}
-                onSubmit={(expense) => {
-                    props.dispatch(editExpense(props.expense.id, expense));
-                    props.history.push('/');
-                    console.log('updated', expense);
-                }}
-            />
-            <button onClick={() => {
-                props.dispatch(removeExpense({ id: props.expense.id }));
-                props.history.push('/');
-            }}>Remove</button>
-        </div>
-    );
+export class EditExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.dispatch(editExpense(this.props.expense.id, expense));
+        this.props.history.push('/');
+    };
+    onRemove = () => {
+        this.props.dispatch(removeExpense({ id: this.props.expense.id }));
+        this.props.history.push('/');
+    };
+    render() {
+        return (
+            <div>
+                <ExpenseForm 
+                    expense={this.props.expense}
+                    onSubmit={this.onSubmit}
+                />
+                <button onClick={this.onRemove}>Remove</button>
+            </div>            
+        );
+    }
 };
+
+// const EditExpensePage = (props) => {
+//     return (
+//         <div>
+//             <ExpenseForm 
+//                 expense={props.expense}
+//                 onSubmit={(expense) => {
+//                     props.dispatch(editExpense(props.expense.id, expense));
+//                     props.history.push('/');
+//                     console.log('updated', expense);
+//                 }}
+//             />
+//             <button onClick={() => {
+//                 props.dispatch(removeExpense({ id: props.expense.id }));
+//                 props.history.push('/');
+//             }}>Remove</button>
+//         </div>
+//     );
+// };
 
 const mapStateToProps = (state, props) => {
     return {
